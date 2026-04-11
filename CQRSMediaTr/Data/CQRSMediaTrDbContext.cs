9b6b29dd230232a7256323e9baf8250d13b1f47b
shivamagrawal02 <shivamagrawal022001@gmail.com>
2026-04-11 16:07:12 +0530

    Added model ,dbcontext, migration, and folders for segregation

---
+++ CQRSMediaTr/Data/CQRSMediaTrDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CQRSMediaTr.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSMediaTr.Data
+{
+    public class CQRSMediaTrDbContext:DbContext
+    {
+        public CQRSMediaTrDbContext(DbContextOptions<CQRSMediaTrDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
